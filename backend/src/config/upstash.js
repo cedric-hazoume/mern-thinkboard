cce37dfcfdf13a8cce406a3774605a904c5269ca
Cedric Hazoume <cedric.hazoume@webbfontaine.com>
2025-09-16 10:04:50 +0100

    updated rate limiter frequency

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Create a new ratelimiter, that allows 10 requests per 20 seconds
+// Create a new ratelimiter, that allows 100 requests per 60 seconds(1 min)
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(100, "60 s"),
